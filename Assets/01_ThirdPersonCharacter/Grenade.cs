3cc6c0467816a6c0e33e9fd4eca9ca70ec20fe2e
LePeruvienn <koto91@free.fr>
2024-09-30 22:34:36 +0200

    Re added grenade

--- Assets/01_ThirdPersonCharacter/Grenade.cs
+++ Assets/01_ThirdPersonCharacter/Grenade.cs
@@ -9,12 +9,15 @@ namespace Starter.ThirdPersonCharacter
 		[Header("Greande Stats")]
         public int maxDamage; // Max damage that the grenade can deal (when player close to explosion)
         public int minDamage; // Min damage taht the grenade can deal (when player far from explosion)
-        public int throwDistance; // Throw distance of the grenade 
+        public int throwForce; // Forward throw force
+        public int throwUpwardForce; // Upward throw force
         public float explosionRadius; // All the radius that the grenade will damage on explode
         public int timeBeforeExplode; // Time before the grenade explode
         
         // Private
         private Transform spawnGrenadePosition; // Where the bullet is gonna spawn
+		private PlayerInventory _playerInventory;
+		private Rigidbody _rigidBody;
         
         public override ItemType getType()
         {
@@ -23,7 +26,42 @@ namespace Starter.ThirdPersonCharacter
 
         public override void use()
         {
-            // TO DO
+			// Getting PlayerInventory
+			if (_playerInventory == null)
+				_playerInventory = GetComponentInParent<PlayerInventory> ();
+
+			// Getting player rigidBody
+			if (_rigidBody == null)
+				_rigidBody = GetComponent<Rigidbody> ();
+
+			// Drop Grenade
+			_playerInventory.dropCurrentSelection ();
+
+			// Getting camera transform
+			Transform cameraTransform = Camera.main.transform;
+
+			// Setting up throwForce toward rotation of the camera
+			Vector3 forceToAdd = (cameraTransform.forward * throwForce) + (cameraTransform.up * throwUpwardForce);
+
+			// Add the force to the grenade rigidBody
+			_rigidBody.AddForce (forceToAdd);
+
+			// Start exploding coroutine
+			StartCoroutine (explodeAfterDelay ());
         }
+
+		private IEnumerator explodeAfterDelay ()
+		{
+			// Wait the delay before explode
+			yield return new WaitForSeconds (timeBeforeExplode);
+
+			// Make the greande explode
+			explode ();
+		}
+
+		private void explode ()
+		{
+			Destroy (gameObject);
+		}
     }
 }
