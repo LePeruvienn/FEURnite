37691824ac214f7a07279427888451e9af045a8b
Miasmax <2016maxe@gmail.com>
2024-10-22 18:11:38 +0200

    fixing the procedural animation for 90% of situation

--- Assets/01_MainScene/Scripts/Aiming/mousePosition3D.cs
+++ Assets/01_MainScene/Scripts/Aiming/mousePosition3D.cs
@@ -1,3 +1,4 @@
+using Starter.ThirdPersonCharacter;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,18 +21,24 @@ public class mousePosition3D : MonoBehaviour
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit[] hits = Physics.RaycastAll(ray, 999f, aimColliderLayerMask);
 
+        float playerDistance = 12f;
+
+
         // Loop through all hits from the ray
         foreach (RaycastHit hit in hits)
-        {
+        {   
+            // Calculate the distance from the camera to the hit point
+            float hitDistance = Vector3.Distance(Camera.main.transform.position, hit.point);
+
             // Check if the current hit is not the one with the "ItemRaycast" tag
-            if (hit.collider.tag != "ItemRaycast")
+            if (hitDistance > playerDistance && hit.collider.tag != "ItemRaycast")
             {
                 // If it's not, set the position to this point
                 transform.position = hit.point;
+                Debug.Log(hitDistance+ "hitDistance");
+                Debug.Log(playerDistance + "playerDistance");
                 break; // Stop checking after the first valid hit
             }
-
-            Debug.Log("You hit an object with tag: " + hit.collider.tag);
         }
     }
 }
