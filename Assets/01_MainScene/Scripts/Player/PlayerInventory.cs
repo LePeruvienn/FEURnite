6e5929401448eb96a8714ef1df95f8601cda780a
LePeruvienn <koto91@free.fr>
2024-12-08 01:31:35 +0100

    Update mult

--- Assets/01_MainScene/Scripts/Player/PlayerInventory.cs
+++ Assets/01_MainScene/Scripts/Player/PlayerInventory.cs
@@ -40,7 +40,7 @@ namespace Starter.ThirdPersonCharacter
 		private GameObject[] _items;
 		[Networked] private int _selectedIndex {get; set;}
 		private bool _canPickUp;
-		[Networked] private GameObject _lastPickableObject {get; set;}
+		private GameObject _lastPickableObject;
 		
 		public override void Spawned()
 		{
