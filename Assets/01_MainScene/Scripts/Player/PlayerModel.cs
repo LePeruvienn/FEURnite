cd55950620f899dee8f9a7d23deb6876fe1fcd2f
LePeruvienn <koto91@free.fr>
2024-12-10 10:17:21 +0100

    Update mult

--- Assets/01_MainScene/Scripts/Player/PlayerModel.cs
+++ Assets/01_MainScene/Scripts/Player/PlayerModel.cs
@@ -106,10 +106,9 @@ namespace Starter.ThirdPersonCharacter
 		public void die ()
 		{
 			Debug.LogWarning ("DIE !!!");
-			/*
+			
 			Player player = GetComponent<Player> ();
 			player.DebugIsDead = true;
-			*/
 		}
 
 		public void heal (int amount) 
