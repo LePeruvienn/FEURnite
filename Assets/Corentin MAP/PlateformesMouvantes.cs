35bc40bc90d3b68d07ebb18709442779c4cef6f7
dory-san <doryan.jovanovic@universite-paris-saclay.fr>
2025-01-15 04:12:49 +0100

    Revert "networking platform : try platform mouvement 1"
    This reverts commit 4ca7a5c4ac905ed9f74141bd6f24afa53c6a1ad7.

--- Assets/Corentin MAP/PlateformesMouvantes.cs
+++ Assets/Corentin MAP/PlateformesMouvantes.cs
@@ -1,60 +1,25 @@
-using Fusion;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace Starter.ThirdPersonCharacter
 {
 
-    public class PlateformesMouvantes : NetworkBehaviour
+    public class PlateformesMouvantes : MonoBehaviour
     {
-        private GameObject platformObject;
+        public float speed = 1.0f; // Vitesse du mouvement
+        public float height = 5.0f; // Amplitude du mouvement
 
-        [Networked]
-        public float speed { get; set; } // Vitesse du mouvement
-        [Networked]
-        public float height { get; set; } // Amplitude du mouvement
+        private Vector3 startPosition;
 
-        [Networked]
-        private Vector3 startPosition { get; set; }
-        [Networked]
-        private Vector3 position { get; set; }
-
-        [Networked]
-        private NetworkObject thePlatform { get; set; }
-        [Networked]
-        private NetworkTransform thePlatformTransform { get; set; }
-
-        [Networked]
-        private float newX { get; set; }
-        [Networked]
-        private float newY { get; set; }
-        [Networked]
-        private float newZ { get; set; }
-
-        public override void Spawned()
+        void Start()
         {
-            base.Spawned();
-
-            platformObject = this.gameObject;
-
-            thePlatform = platformObject.gameObject.GetComponent<NetworkObject>();
-            thePlatformTransform = platformObject.gameObject.GetComponent<NetworkTransform>();
-            speed = 1.0f;
-            height = 5.0f;
             startPosition = transform.position; // Sauvegarde la position de départ
         }
 
-        public override void Render()
+        void Update()
         {
             // Calcule une nouvelle position en oscillant de haut en bas
-            newX = transform.position.x;
-            newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
-            newZ = transform.position.z;
-
-            position = new Vector3(newX, newY, newZ);
-
-            transform.position = position;
+            float newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
         }
     }
 }
