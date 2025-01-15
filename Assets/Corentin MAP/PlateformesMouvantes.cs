a5c2761f0d0ee9b87846fb0fbaff06398798d711
dory-san <doryan.jovanovic@universite-paris-saclay.fr>
2025-01-15 03:20:56 +0100

    networking platform : try platform mouvement 4

--- Assets/Corentin MAP/PlateformesMouvantes.cs
+++ Assets/Corentin MAP/PlateformesMouvantes.cs
@@ -25,12 +25,12 @@ namespace Starter.ThirdPersonCharacter
         [Networked]
         private NetworkTransform thePlatformTransform { get; set; }
 
-        [Networked]
-        private float newX { get; set; }
-        [Networked]
-        private float newY { get; set; }
-        [Networked]
-        private float newZ { get; set; }
+        //[Networked]
+        //private float newX { get; set; }
+        //[Networked]
+        //private float newY { get; set; }
+        //[Networked]
+        //private float newZ { get; set; }
 
         public override void Spawned()
         {
@@ -48,13 +48,24 @@ namespace Starter.ThirdPersonCharacter
         public override void Render()
         {
             // Calcule une nouvelle position en oscillant de haut en bas
-            newX = transform.position.x;
-            newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
-            newZ = transform.position.z;
+            //newX = transform.position.x;
+            //newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
+            //newZ = transform.position.z;
 
-            position = new Vector3(newX, newY, newZ);
+            //position = new Vector3(newX, newY, newZ);
 
-            transform.position = position;
+            //transform.position = position;
+
+            
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            float newY = startPosition.y + Mathf.Sin(Time.time * speed) * height;
+            Vector3 newPosition = new Vector3(transform.position.x, newY, transform.position.z);
+
+            // Synchronisez la position avec NetworkTransform
+            thePlatformTransform.Teleport(newPosition);
         }
     }
 }
